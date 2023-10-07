cf73e6a7cc045b9261cf1a623bc96a8b9d621fdc
LucSal6868 <125786165+LucSal6868@users.noreply.github.com>
2023-10-07 00:34:59 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+#define BOARDX 3
+#define BOARDY 3
+
 int main(void)
 {
-    printf("helloworld");
-    printf("test2");
-    printf("this is a super test");
+    int **board = malloc(sizeof(int*) * BOARDY);
+    for(int i = 0; i < BOARDY; i++)
+    {
+        board[i] = malloc(sizeof(int) * BOARDX);
+        
+    } 
+
+    
 }
