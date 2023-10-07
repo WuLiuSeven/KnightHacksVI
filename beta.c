7e2271878069ae1931378a4ea42f6deceddb6a24
albos7 <andres.mateo.albornoz@gmail.com>
2023-10-07 11:38:00 -0400

    Finished TicTAcToe

--- beta.c
+++ beta.c
@@ -1,31 +1,80 @@
+
+// -  -
+// ----
 #include <stdio.h>
 
-/*int horizontalWin(char arr[3][3]){
+int horizontalWin(char arr[3][3]) {
+    int numRows = 3;
+    int numCols = 3;
 	int i=0;
 	int j=0;
-	int numRows = 3;
-	int numCols = 3;
-	static char temp;
-	int res=0;
-	for(i=0; i<numRows; i++){
-		for(j=0; j<numCols; j++){
-			if(j==0){
-				temp= arr[i][j];
-			}
-			if(temp==arr[i][j]){
-				res+=1;
-			}
+    for (i = 0; i < numRows; i++) {
+        char temp = arr[i][0]; // Initialize temp with the first element of the row
+        if(temp == ' '){
+        	temp='N';
 		}
-		if(res==2){
-			if(temp !=' '){
-				return res;
-			}
-		}
-		res=0;
+        int consecutiveCount = 1;
+
+        for (j = 1; j < numCols; j++) {
+            if (temp == arr[i][j]) {
+                consecutiveCount++;
+            } else {
+                consecutiveCount = 1; // Reset count if symbols don't match
+                temp = arr[i][j];    // Update temp for the next check
+            }
+        }
+
+        if (consecutiveCount >= 3) {
+            return 1; // Return 1 to indicate a win
+        }
+    }
+
+    return 0; // No horizontal win
+}
+
+int verticalWin(char arr[3][3]) {
+    int numRows = 3;
+    int numCols = 3;
+    int i, j;
+    
+    for (i = 0; i < numCols; i++) { // Iterate through columns
+        char temp = arr[0][i]; // Initialize temp with the first element of the column
+        if (temp == ' ') {
+            temp = 'N';
+        }
+        int consecutiveCount = 1;
+
+        for (j = 1; j < numRows; j++) { // Iterate through rows in the same column
+            if (temp == arr[j][i]) { // Compare elements in the same column
+                consecutiveCount++;
+            } else {
+                consecutiveCount = 1; // Reset count if symbols don't match
+                temp = arr[j][i];    // Update temp for the next check
+            }
+        }
+
+        if (consecutiveCount >= 3) {
+            return 1; // Return 1 to indicate a win
+        }
+    }
+
+    return 0; // No vertical win
+}
+
+int diagonalWin(char arr[3][3]){
+	char temp = arr[1][1]; //Initialize the mid piece
+	if (temp == ' ') {
+            temp = 'N';
+    }
+	int consecutiveCount = 1;
+	if(arr[0][0]==temp && arr[2][2]==temp){
+		return 1;
+	}else if(arr[2][0]==temp && arr[0][2]== temp){
+		return 1;
 	}
 	return 0;
-	
-} */
+}
+
 // Function to display the Tic-Tac-Toe board
 void displayBoard(char board[3][3]) {
     printf("   0   1   2\n"); // Column indices
@@ -81,12 +130,15 @@ int main() {
         // Switch to the next player
         currentPlayer = 1 - currentPlayer;
         // Check for a win or a draw (you can implement this logic in a separate function)
-        //if(horizontalWin(board[3][3])==2){
-        //	printf("wins");
-        //	break;
-		//}
         turn+=1;
-        if(turn>=9){
+        if(turn>=5 && horizontalWin(board)==1){
+        	break;
+		}else if(turn>=5 && verticalWin(board)==1){
+        	break;
+		}else if(turn>=5 && diagonalWin(board)==1 && board[1][1]!=' '){
+			break;
+		}
+		if(turn>=9){
         	gameOver+=1; // If the game is over, set gameOver to 1
 		}
     }
