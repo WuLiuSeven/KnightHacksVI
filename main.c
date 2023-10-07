68a52163ebf061263b1c4868a18b8b8b3fbbc913
LucSal6868 <125786165+LucSal6868@users.noreply.github.com>
2023-10-07 09:42:37 -0400

    Update main.c

--- main.c
+++ main.c
@@ -2,42 +2,53 @@
 #include <stdlib.h>
 
 
-#define BOARDX 3
-#define BOARDY 3
+#define BOARD 9
 
 ///////////////////////////////////////////////////////////
 
-void printBoard(int **board)
+void printBoard(int b[])
 {
-    for(int i = 0; i < BOARDY; i++)
-    {
-        for(int j = 0; j < BOARDX; j++)
-            printf("%d ", board[i][j]);
-
-        printf("\n");
-    } 
+    printf("%d %d %d\n", b[0], b[1], b[2]);
+    printf("%d %d %d\n", b[3], b[4], b[5]);
+    printf("%d %d %d\n", b[6], b[7], b[8]);
 }
 
-int** permuteBoard(int **b, int pos[])
+void permuteBoard(int b[], int turn, int useable)
 {
+    //Player turn and symbol
+    int player;
+    if(turn == 0)
+        player = '1';
+    else
+        player = '2';
     
+    //break Case
+    if(useable == 0)
+        //PRINT BOARD
+        return 0;
+
+    //Permute Check
+    for(int i = 0; i < useable; i++)
+    {
+        int j = i;
+        while(b[j] != 0)
+            j++;
+
+        b[j] = player;
+        printBoard(b);
+        permuteBoard(b, (turn + 1)%2, useable - 1);
+        b[j] = 0;
+    }
+
 }
 
 ///////////////////////////////////////////////////////////
 
 int main(void)
 {
-    int **board = malloc(sizeof(int*) * BOARDY);
-    for(int i = 0; i < BOARDY; i++)
-    {
-        board[i] = malloc(sizeof(int) * BOARDX);
-        
-        for(int j = 0; j < BOARDX; j++)
-            board[i][j] = 0;
-    } 
+    int board[BOARD] = {0};
 
-    int pos[2] = {0}
-    permuteBoard(board, pos);
+    permuteBoard(board, 0, BOARD);
 }
 
 ///////////////////////////////////////////////////////////
