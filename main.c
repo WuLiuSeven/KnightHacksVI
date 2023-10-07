85ff53c9a930bc11875805cbafad64b199925808
WuLiuSeven <125783608+WuLiuSeven@users.noreply.github.com>
2023-10-07 09:28:14 -0400

    Test Branch 0
    Testing the branch from Main Branch

--- main.c
+++ main.c
@@ -5,6 +5,34 @@
 #define BOARDX 3
 #define BOARDY 3
 
+<<<<<<< Updated upstream
+=======
+// 0 = nothing, 1 = x, 2 = O
+///////////////////////////////////////////////////////////
+
+void printBoard(int **board)
+{
+    //column
+    for(int i = 0; i < BOARDX; i++)
+    {
+        //row
+        for (int j = 0; j < BOARDY; j++) 
+        {
+            if board
+
+            printf("\n");
+        }
+    } 
+}
+
+int** permuteBoard(int **b, int pos[])
+{
+    
+}
+
+///////////////////////////////////////////////////////////
+
+>>>>>>> Stashed changes
 int main(void)
 {
     int **board = malloc(sizeof(int*) * BOARDY);
